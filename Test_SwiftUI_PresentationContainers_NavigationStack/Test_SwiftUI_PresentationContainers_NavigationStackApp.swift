f3408cf2f413a43a41c8290aecedaf57f0750f9a
JTrojanowicz <jaroslaw.trojanowicz@gmail.com>
2022-07-20 13:14:47 +0200

    Initial Commit

---
+++ Test_SwiftUI_PresentationContainers_NavigationStack/Test_SwiftUI_PresentationContainers_NavigationStackApp.swift
@@ -0,0 +1,17 @@
+//
+//  Test_SwiftUI_PresentationContainers_NavigationStackApp.swift
+//  Test_SwiftUI_PresentationContainers_NavigationStack
+//
+//  Created by Jaroslaw Trojanowicz on 20/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct Test_SwiftUI_PresentationContainers_NavigationStackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
